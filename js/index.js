bb77ec67208d3e20c52593bbee15bc611793a7b4
Forhad96 <forhadairdrop@gmail.com>
2023-08-18 15:34:05 +0600

    rectangel area calculation

--- js/index.js
+++ js/index.js
@@ -1,19 +1,31 @@
 // reusable function ===> reduce duplicate code
-function getInputValue(fieldId){
-    const inputField = document.getElementById(fieldId);
-    const inputValueText = inputField.value;
-    const value = parseFloat(inputValueText);
-    return value; 
+function getInputValue(fieldId) {
+  const inputField = document.getElementById(fieldId);
+  const inputValueText = inputField.value;
+  const value = parseFloat(inputValueText);
+  return value;
 }
 
-function triangleAreaCalculate(){
-    const base = getInputValue('triangle_base')
-    const height = getInputValue('triangle_height');
-    const area = 0.5 * base * height;
+//function for Triangle area calculate
 
-    console.log("🚀 ~ file: index.js:14 ~ triangleAreaCalculate ~ area:", area)
-    // show area total
-    const triangleArea = document.getElementById('triangle_area');
-    triangleArea.innerText = area;
-    
+function triangleAreaCalculate() {
+  const base = getInputValue("triangle_base");
+  const height = getInputValue("triangle_height");
+  const area = 0.5 * base * height;
+
+  // show area total
+  const triangleArea = document.getElementById("triangle_area");
+  triangleArea.innerText = area;
 }
+
+// function for rectangle area calculate
+function rectangleAreaCalculate() {
+  const width = getInputValue("rectangle_width");
+  const height = getInputValue("rectangle_length");
+  const area = width * height;
+
+  const rectangleArea = document.getElementById('rectangle_area');
+  rectangleArea.innerText = area; 
+}
+
+// function for rectangle area calculate
