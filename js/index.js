3c08f5c3832f39ebcb2f79c07b523aff27ff4b55
Forhad96 <forhadairdrop@gmail.com>
2023-08-18 16:04:44 +0600

    paralleloram area calculation

--- js/index.js
+++ js/index.js
@@ -5,7 +5,11 @@ function getInputValue(fieldId) {
   const value = parseFloat(inputValueText);
   return value;
 }
+function setElementInnerText(elementId,area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 
+}
 //function for Triangle area calculate
 
 function triangleAreaCalculate() {
@@ -13,9 +17,8 @@ function triangleAreaCalculate() {
   const height = getInputValue("triangle_height");
   const area = 0.5 * base * height;
 
-  // show area total
-  const triangleArea = document.getElementById("triangle_area");
-  triangleArea.innerText = area;
+  // show triangle area total
+  setElementInnerText('triangle_area',area);
 }
 
 // function for rectangle area calculate
@@ -23,9 +26,15 @@ function rectangleAreaCalculate() {
   const width = getInputValue("rectangle_width");
   const height = getInputValue("rectangle_length");
   const area = width * height;
+  setElementInnerText('rectangle_area',area);
 
-  const rectangleArea = document.getElementById('rectangle_area');
-  rectangleArea.innerText = area; 
 }
 
 // function for rectangle area calculate
+function parallelogramAreaCalculate() {
+    const base = getInputValue("parallelogram_base");
+    const height = getInputValue("parallelogram_height");
+    const area = base * height;
+  
+    setElementInnerText('parallelogram_area',area);
+  }
